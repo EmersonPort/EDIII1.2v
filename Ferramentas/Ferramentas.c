6c70f2e3228ee0169fe7157eecdb76621be35c9f
EmersonPort <68072471+EmersonPort@users.noreply.github.com>
2021-11-02 19:11:23 -0300

    Add files via upload

--- Ferramentas/Ferramentas.c
+++ Ferramentas/Ferramentas.c
@@ -27,7 +27,6 @@ void getstr(FILE* file1,char* string){//pega uma string do  aquivo csv
     }
     s[i]='\0';
     strncpy(string,s,i+1);
-    //printf("%s\n",string);
 }
 
 int potencia(int n,int pot){ // retorna n^(pot) com pot um inteiro positivo
@@ -60,7 +59,7 @@ int getint(int* p,FILE* file ) { // pega um inteiro de um arquivo csv
 void insereg(FILE* file,Dados* d,int flag){// insere um registro
     long int tamanho_nomeEstacao =strlen(d->nomeEstacao);
     long int tamanho_nomeLinha =strlen(d->nomeLinha);
-    (d->nomeEstacao)[tamanho_nomeEstacao]='|';
+    (d->nomeEstacao)[tamanho_nomeEstacao]='|';// poe o | para insercao
     (d->nomeLinha)[tamanho_nomeLinha]='|';
     tamanho_nomeEstacao++;
     tamanho_nomeLinha++;
@@ -69,7 +68,7 @@ void insereg(FILE* file,Dados* d,int flag){// insere um registro
     if(flag==0){// flag==0 -> ha limitacao de tamanho
         int espaco;
         fwrite(&(d->removido),sizeof(char),1,file);
-        fread(&espaco,sizeof(int),1,file);
+        fread(&espaco,sizeof(int),1,file);// Faz a leitura(fread) do espaco disponivel
         fwrite(&(d->proxLista),sizeof(long int),1,file);
 
         fwrite(&(d->codEstacao),sizeof(int),1,file);
@@ -78,13 +77,13 @@ void insereg(FILE* file,Dados* d,int flag){// insere um registro
         fwrite(&(d->distProxEstacao),sizeof(int),1,file);    
         fwrite(&(d->codLinhaIntegra),sizeof(int),1,file);    
         fwrite(&(d->codEstIntegra),sizeof(int),1,file);
-        espaco= espaco- d->tamanhoRegistro;
+        espaco= espaco- d->tamanhoRegistro;//Agora , o espaco que resta sera usado pelo trash
         
         fwrite(d->nomeEstacao,sizeof(char),tamanho_nomeEstacao,file);
         fwrite(d->nomeLinha,sizeof(char),tamanho_nomeLinha,file);
         fill_trash(file,espaco); }
     else{// flag==1 -> n ha limitacao de tamanho
-        fwrite(&(d->removido),sizeof(char),1,file);
+        fwrite(&(d->removido),sizeof(char),1,file);//Simplesmente escreve todos os dados no arquivo
         fwrite(&(d->tamanhoRegistro),sizeof(int),1,file);
         fwrite(&(d->proxLista),sizeof(long int),1,file);
         fwrite(&(d->codEstacao),sizeof(int),1,file);
@@ -99,32 +98,30 @@ void insereg(FILE* file,Dados* d,int flag){// insere um registro
     }
 }
 
-void limpad(Dados* d){
+void limpad(Dados* d){// Deixa os dados do registro d zerados(Apenas os que podem assumir esse valor)
     d->codLinha=-1;
     strcpy(d->nomeLinha,"");
     d->codProxEstacao=-1;
     d->distProxEstacao=-1;
     d->codLinhaIntegra=-1;
-    d->codEstIntegra=-1;
-
-}
+    d->codEstIntegra=-1;}
 
 
-void printint(int x){
+void printint(int x){// Imprime o valor , caso nao seja nulo
     if(x==-1)printf("NULO ");
     else{printf("%i ",x);}
 }
 
 
-int pegaRegistro(Dados* d,FILE* file){
+int pegaRegistro(Dados* d,FILE* file){//Pega o registro seguinte e poe em d
     char string[50];
     char* sub;
-    if(fread(&(d->removido),sizeof(char),1,file)!=1) return 0;
+    if(fread(&(d->removido),sizeof(char),1,file)!=1) return 0;//caso nao consiga puxar dados do aquivo retorna 0 (ERRO)
     fread(&(d->tamanhoRegistro),sizeof(int),1,file);
     
     if (d->removido=='1'){
-        fseek(file,d->tamanhoRegistro,SEEK_CUR);
-        return -1;}
+        fseek(file,d->tamanhoRegistro,SEEK_CUR);//Se estiver removido pula pro proximo
+        return -1;}// -1 indica que o registro foi pulado
     fread(&(d->proxLista),sizeof(long int),1,file);
     fread(&(d->codEstacao),sizeof(int),1,file);
     fread(&(d->codLinha),sizeof(int),1,file);    
@@ -133,16 +130,16 @@ int pegaRegistro(Dados* d,FILE* file){
     fread(&(d->codLinhaIntegra),sizeof(int),1,file);    
     fread(&(d->codEstIntegra),sizeof(int),1,file);
     fread(string,sizeof(char),d->tamanhoRegistro-32,file);
-    sub=strtok(string,"|");
+    sub=strtok(string,"|");// Leitura ate o |
     strcpy(d->nomeEstacao,sub);
     sub=strtok(NULL,"|");
-    if(sub==NULL) strcpy(d->nomeLinha,"");
+    if(sub==NULL) strcpy(d->nomeLinha,"");// sub recebe NULL caso tenha ||
     else{strcpy(d->nomeLinha,sub);} 
-    return 1;
+    return 1;//Tudo ok
 
 }
 
-void imprimeRegistro(Dados* d){
+void imprimeRegistro(Dados* d){// imprime todos os dados do registro
         printf("%i ",d->codEstacao);     
 
         if(strlen(d->nomeEstacao)==0) printf("NULO ");
@@ -162,12 +159,10 @@ void imprimeRegistro(Dados* d){
 }
 
 
-void restrinja(Dados* d){
+void restrinja(Dados* d){//restrinje os dados do registro
     char campobuscado[15];
     scanf(" %s ",campobuscado);
-    //fgets(campobuscado,15,stdin);
-    //printf("%s\n",campobuscado);
-
+    // dependendo do campobuscado o scan sera diferente
     if(strcmp(campobuscado,"codEstacao")==0) scanf("%i ",&(d->codEstacao));
     else if(strcmp(campobuscado,"nomeEstacao")==0) scan_quote_string(d->nomeEstacao);
     else if(strcmp(campobuscado,"codLinha")==0) scanf("%i ",&(d->codLinha));
@@ -175,14 +170,12 @@ void restrinja(Dados* d){
     else if(strcmp(campobuscado,"codProxEstacao")==0) scanf("%i ",&(d->codProxEstacao)); 
     else if(strcmp(campobuscado,"distProxEstacao")==0) scanf("%i ",&(d->distProxEstacao));
     else if(strcmp(campobuscado,"codLinhaIntegra")==0) scanf("%i ",&(d->codLinhaIntegra));
-    else if(strcmp(campobuscado,"codEstIntegra")==0) scanf("%i ",&(d->codEstIntegra));
-    //printf("%s  %i  %s\n",d->nomeEstacao,d->codEstacao,d->nomeLinha);
-} 
+    else if(strcmp(campobuscado,"codEstIntegra")==0) scanf("%i ",&(d->codEstIntegra));} 
 
 
-void generalize(Dados* d){
-    d->codEstacao=-2;
-    strcpy(d->nomeEstacao,"Sem restrição");
+void generalize(Dados* d){// Generaliza os dados : -2 eh tido como valor sem restricao
+    d->codEstacao=-2;// -2 nao conta como elemento buscado
+    strcpy(d->nomeEstacao,"Sem restrição");// Sem restricao tb n
     d->codLinha=-2;
     strcpy(d->nomeLinha,"Sem restrição");
     d->codProxEstacao=-2;
@@ -190,8 +183,8 @@ void generalize(Dados* d){
     d->codLinhaIntegra=-2;
     d->codEstIntegra=-2;}
 
-int compativel(Dados* r,Dados* d){
-    if(d->codEstacao!=-2 && d->codEstacao!=r->codEstacao) return 0;
+int compativel(Dados* r,Dados* d){// retorna 1 se r eh compativel com d e 0 se n for
+    if(d->codEstacao!=-2 && d->codEstacao!=r->codEstacao) return 0;// Se d n tiver valor definido (tudo igual a -2) qualquer r eh compativel
     if(strcmp(d->nomeEstacao,"Sem restrição")!=0 && strcmp(d->nomeEstacao, r->nomeEstacao)!=0) return 0;
     if(d->codLinha!=-2 && d->codLinha!=r->codLinha) return 0;
     if(strcmp(d->nomeLinha,"Sem restrição")!=0 && strcmp(d->nomeLinha, r->nomeLinha)!=0) return 0;
@@ -205,30 +198,30 @@ int compativel(Dados* r,Dados* d){
 
 
 void firstfit(FILE* file,Dados* d,long int* topolista){
-    long int bts_anterior;
+    long int bts_anterior;//byte offsets
     long int bts_atual=1;
     long int bts_prox=*topolista;
     Dados* r = (Dados*) malloc(sizeof(Dados));
 
-    while(1){
-        if(bts_prox==-1){
+    while(1){//O while fica ate ter um return
+        if(bts_prox==-1){// So poe no final e n muda a lista
             fseek(file,0,SEEK_END);
-            insereg(file,d,1);
+            insereg(file,d,1);//flag=1 pq n tem limite de tamnho
             free(r);
             return;}
 
-        fseek(file,bts_prox,SEEK_SET);
+        fseek(file,bts_prox,SEEK_SET);//leitura do proximo byte offset
         fread(&(r->removido),sizeof(char),1,file);
         fread(&(r->tamanhoRegistro),sizeof(int),1,file);
         fread(&(r->proxLista),sizeof(long int),1,file);
-        bts_anterior = bts_atual;
+        bts_anterior = bts_atual;//atualiza o byte offset
         bts_atual = bts_prox;
         bts_prox = r->proxLista;
         if(r->tamanhoRegistro>=d->tamanhoRegistro) {
-            if(bts_anterior==1){
+            if(bts_anterior==1){// 1 eh bts de cabecalho
                 *topolista=bts_prox;}
             else {
-                fseek(file,bts_anterior+5,SEEK_SET);
+                fseek(file,bts_anterior+5,SEEK_SET);//mudando o prox bts do registro anterior
                 fwrite(&(bts_prox),sizeof(long int),1,file);}
 
             fseek(file,bts_atual,SEEK_SET);
@@ -237,17 +230,17 @@ void firstfit(FILE* file,Dados* d,long int* topolista){
             return;}}}
 
 
-int intteclado(FILE* file){
+int intteclado(FILE* file){// leitura de inteiro do teclado, se n for "NULO".
     char c;
     int soma=0;
     scanf(" %c",&c);
-    if(c=='N' || c=='n') {
+    if(c=='N' || c=='n') {//pegando string NULO
         getchar();
         getchar();
         getchar();
         return -1;}
     soma= (int) (c - '0');
-    while(c=getchar(),c!=' ' && c!='\r' && c!='\n'){
+    while(c=getchar(),c!=' ' && c!='\r' && c!='\n'){// pegando numero
         soma= soma*10;
         soma += (int) (c - '0');}
     return soma;
@@ -256,25 +249,25 @@ int intteclado(FILE* file){
 
 
 void atualiza_ca(FILE* file,Cabecalho* pca){
-    fseek(file,17,SEEK_SET);
+    fseek(file,17,SEEK_SET);//bts q termina o cabecalho
     pca->nroEstacoes=0;
     pca->nroParesEstacao=0;
-    char nomesEstacoes[500][40];
-    char pares[500][500]={0};
+    char nomesEstacoes[500][40];//matriz de nomes de estacoes
+    char pares[500][500]={0};// matriz de pares zerada
     Dados* d = (Dados*) malloc(sizeof(Dados));
     int status;
         while((status=pegaRegistro(d,file))!=0){
             if (status==-1) continue;
             int i=0;
             for(;i<pca->nroEstacoes;i++) if(strcmp(nomesEstacoes[i],d->nomeEstacao)==0) break;
-            if(i==pca->nroEstacoes) {
-                strcpy(nomesEstacoes[i],d->nomeEstacao);
+            if(i==pca->nroEstacoes) {//:se tiver terminado o for sem o break
+                strcpy(nomesEstacoes[i],d->nomeEstacao);// aumenta a lista de estacoes
                 pca->nroEstacoes++;}
-            if(d->codProxEstacao!=-1 && pares[d->codEstacao][d->codProxEstacao]!='1') {
-                    pares[d->codEstacao][d->codProxEstacao]='1';
-                    pca->nroParesEstacao++;}        
+            if(d->codProxEstacao!=-1 && pares[d->codEstacao][d->codProxEstacao]!='1') {// Se o par n tiver marcado
+                    pares[d->codEstacao][d->codProxEstacao]='1';//marca o par
+                    pca->nroParesEstacao++;}  // add o par      
             }
-    fseek(file,0,SEEK_SET);
+    fseek(file,0,SEEK_SET);//volta e atualiza o cabecalho no arquivo
     fwrite(&(pca->status),sizeof(char),1,file);
     fwrite(&(pca->topoLista),sizeof(long int),1,file);
     fwrite(&(pca->nroEstacoes),sizeof(int),1,file);
--- IO/IO.c
+++ IO/IO.c
@@ -12,7 +12,6 @@
 
 void aplicacao1(FILE* file1,FILE* file2){
     fseek(file1,101,SEEK_SET);//posicao em que comeca as iformacoes que serao lidas
-    //fseek(file2,17,SEEK_SET);
     Dados* d = (Dados*) malloc(sizeof(Dados));// d armazena o registro q sera lido e armazenado
     d->removido='0';
     d->proxLista=-1;
@@ -26,31 +25,19 @@ void aplicacao1(FILE* file1,FILE* file2){
     fwrite(&(ca.topoLista),sizeof(long int),1,file2);
     fwrite(&(ca.nroEstacoes),sizeof(int),1,file2);
     fwrite(&(ca.nroParesEstacao),sizeof(int),1,file2);
-    //short int vestacoes[1000]={0};// armazena os numEstacoes setados
     char c;// carac. que eh usado para pegar o \n
     while(getint(&(d->codEstacao),file1)==1){// enquanto tiver leitura
-        //printf("%d\n",d->codEstacao);
-        /*if(vestacoes[d->codEstacao]==0){// se essa estacao n estiver setada , ela fica
-            vestacoes[d->codEstacao]=1;
-            ca.nroEstacoes++;}// +estacoes , pois ha uma estacao nova*/
         getstr(file1,d->nomeEstacao);
         getint(&(d->codLinha),file1);
-        //printf("%d\n",d->codLinha);
         getstr(file1,d->nomeLinha);
         getint(&(d->codProxEstacao),file1);
-        //printf("%d\n",d->codProxEstacao);
         getint(&(d->distProxEstacao),file1);
-        //printf("%d\n",d->distProxEstacao);
         getint(&(d->codLinhaIntegra),file1);
-        //printf("%d\n",d->codLinhaIntegra);
         getint(&(d->codEstIntegra),file1);
-        //printf("%d\n",d->codEstIntegra);
         fread(&c,sizeof(char),1,file1);// pega o \n
-        int j=0;
         insereg(file2,d,1);//insere registro sem limitacao de espaco
         limpad(d);}
-
-    fseek(file2,0,SEEK_SET);//voltamos para atualizar o cabecalho 
+ 
     ca.status='1';
     atualiza_ca(file2,&ca);
     free(d);}
@@ -58,16 +45,13 @@ void aplicacao1(FILE* file1,FILE* file2){
 
 void aplicacao2(FILE* file){
     fseek(file,17,SEEK_SET);
-    //printf("%c %li %i %i \n",ca.status,ca.topoLista,ca.nroEstacoes,ca.nroParesEstacao);
     Dados* d = (Dados*) malloc(sizeof(Dados));
     int flag=1;
     int status;
     while((status=pegaRegistro(d,file))!=0){
-        //printf("%c %i %li ",d->removido,d->tamanhoRegistro,d->proxLista);
-        if(status==-1) continue;
-        flag=0;
-        imprimeRegistro(d);
-        }
+        if(status==-1) continue;//registro removido
+        flag=0;//Algum registro valido foi lido
+        imprimeRegistro(d);}
     free(d);
     if(flag==1)printf("Registro inexistente.\n");}
 
@@ -75,25 +59,17 @@ void aplicacao2(FILE* file){
 void aplicacao3(FILE* file){
     int n;
     scanf("%i\n",&n);
-    Dados* d = (Dados*) malloc(sizeof(Dados));
-    Dados* r = (Dados*) malloc(sizeof(Dados));
+    Dados* d = (Dados*) malloc(sizeof(Dados));// dados que serao buscados
+    Dados* r = (Dados*) malloc(sizeof(Dados));// registros que serao lidos
     generalize(d);
     
-    for(int i=0;i<n;i++) restrinja(d);
-    //int codEstacao=0;
-    //char nomeEstacao[40];
-    //char nomeLinha[40];
-
-    //char cod[]="codEstacao";
-    //char nEstacao[]="nomeEstacao";
-    //char nLinha[]="nomeLinha";
-    
+    for(int i=0;i<n;i++) restrinja(d); // executa n restricoes
     int status=0;
     int flag=1;
     fseek(file,17,SEEK_SET);
     while((status=pegaRegistro(r,file))!=0){
-        if (status==-1 || compativel(r,d)!=1) continue;      
-        flag=0;
+        if (status==-1 || compativel(r,d)!=1) continue; //registro removido ou invalido     
+        flag=0;//Algum registro valido foi lido
         imprimeRegistro(r);}
 
 free(d);
@@ -109,46 +85,30 @@ void aplicacao4(FILE* file){
     Dados* d = (Dados*) malloc(sizeof(Dados));
     Dados* r = (Dados*) malloc(sizeof(Dados));
     Cabecalho ca;
-    fread(&(ca.status),sizeof(char),1,file);
+    fread(&(ca.status),sizeof(char),1,file);//leitura do cabecalho
     fread(&(ca.topoLista),sizeof(long int),1,file);
     fread(&(ca.nroEstacoes),sizeof(int),1,file);
     fread(&(ca.nroParesEstacao),sizeof(int),1,file);
     
     for(int i=0;i<n;i++){
-        if(i>0) fseek(file,17,SEEK_SET);
+        if(i>0) fseek(file,17,SEEK_SET);//volta ao inicio dos registros
         generalize(d);
         scanf("%i ",&x);
-        for(int j=0;j<x;j++) restrinja(d);
+        for(int j=0;j<x;j++) restrinja(d);// executa x restricoes
         int status;
         while((status=pegaRegistro(r,file))!=0){
-            if (status==-1 || compativel(r,d)!=1) continue;
+            if (status==-1 || compativel(r,d)!=1) continue; //registro removido ou invalido   
             r->removido='1';
-            r->proxLista=ca.topoLista;
-            //if(r->codProxEstacao!=-1) ca.nroParesEstacao--;
-            //printf("%ld\n",ftell(file));
-            ca.topoLista = ftell(file)-r->tamanhoRegistro -5;
+            r->proxLista=ca.topoLista;//botando r no topo da lista
+            ca.topoLista = ftell(file)-r->tamanhoRegistro -5;//bts atual - tamanho registro -(int do campo do tamanho)- char do removido
             fseek(file,ca.topoLista,SEEK_SET);
-            //printf("%d %ld\n",r->tamanhoRegistro,ftell(file));
-            //printf("%c %d %ld",r->removido,r->tamanhoRegistro,r->proxLista);
-            //printf("%ld\n",ca.topoLista);
             fwrite(&(r->removido),sizeof(char),1,file);
             fwrite(&(r->tamanhoRegistro),sizeof(int),1,file);
             fwrite(&(r->proxLista),sizeof(long int),1,file);
-            //printf("\t\t%c %d %ld\n",r->removido,r->tamanhoRegistro,r->proxLista);
-            fseek(file,r->tamanhoRegistro-8,SEEK_CUR);}}
-
-
+            fseek(file,r->tamanhoRegistro-8,SEEK_CUR);//vai para o final do registro
+            }}
     free(d);
     free(r);
-    //ca.nroEstacoes=0;
-    /*ca.nroParesEstacao=0;
-    for(int i=0;i<500;i++){
-        int somadepares=0;
-        for(int j=1;j<500;j++){
-            somadepares+=Matestacoes[i][j];}
-        ca.nroParesEstacao+=somadepares;
-        somadepares+=Matestacoes[i][0];}*/
-        //if(somadepares>0) ca.nroEstacoes++;}
     atualiza_ca(file,&ca);}
 
 void aplicacao5(FILE* file){
@@ -162,17 +122,17 @@ void aplicacao5(FILE* file){
     Dados* d = (Dados*) malloc(sizeof(Dados));
     d->removido='0';
     d->proxLista=-1;
-    for(int i=0;i<n;i++) {
+    for(int i=0;i<n;i++) {//pega todos os dados do teclado n vezes
     d->codEstacao=intteclado(file);
     scan_quote_string(d->nomeEstacao);
     d->codLinha=intteclado(file);
     scan_quote_string(d->nomeLinha);
-    d->tamanhoRegistro=34+ strlen(d->nomeEstacao) + strlen(d->nomeLinha);
+    d->tamanhoRegistro=34+ strlen(d->nomeEstacao) + strlen(d->nomeLinha);// 32 + 2*| + as strings
     d->codProxEstacao=intteclado(file);
     d->distProxEstacao=intteclado(file);
     d->codLinhaIntegra=intteclado(file);
     d->codEstIntegra=intteclado(file);
-    if(i!=n-1) while(getchar()!='\n');
+    if(i!=n-1) while(getchar()!='\n');// Se n for a ultima linha fica esperando o \n
     firstfit(file,d,&(ca.topoLista));}
     atualiza_ca(file,&ca);    
     free(d);}
@@ -188,11 +148,11 @@ void aplicacao6(FILE* file){
     fread(&(ca.topoLista),sizeof(long int),1,file);
     fread(&(ca.nroEstacoes),sizeof(int),1,file);
     fread(&(ca.nroParesEstacao),sizeof(int),1,file);
-    Dados* buscado = (Dados*) malloc(sizeof(Dados));
-    Dados* atualizado = (Dados*) malloc(sizeof(Dados));
-    Dados* r = (Dados*) malloc(sizeof(Dados));
+    Dados* buscado = (Dados*) malloc(sizeof(Dados));//dados buscados
+    Dados* atualizado = (Dados*) malloc(sizeof(Dados));//dados que serao atualizadas
+    Dados* r = (Dados*) malloc(sizeof(Dados));//registro que sera lido e possivelmente atualizado
     for(int i=0;i<n;i++) {
-        if(i>0) fseek(file,17,SEEK_SET);
+        if(i>0) fseek(file,17,SEEK_SET);//volta ao inicio dos registros
         generalize(buscado);
         scanf("%i ",&x);
         for(int j=0;j<x;j++) restrinja(buscado);
@@ -202,9 +162,9 @@ void aplicacao6(FILE* file){
         int status;
         while((status=pegaRegistro(r,file))!=0){
             if (status==-1) continue;
-            if (compativel(r,buscado)!=1) continue;
-            if(atualizado->codEstacao!=-2) r->codEstacao=atualizado->codEstacao;
-            if(atualizado->codLinha!=-2) r->codLinha=atualizado->codLinha;
+            if (compativel(r,buscado)!=1) continue;// r tem que ser compatil com o que eh buscado
+            if(atualizado->codEstacao!=-2) r->codEstacao=atualizado->codEstacao;// se tiver restricao no atualizado ,entao atualiza o r
+            if(atualizado->codLinha!=-2) r->codLinha=atualizado->codLinha;// analogamente
             if(atualizado->codProxEstacao!=-2) r->codProxEstacao=atualizado->codProxEstacao;
             if(atualizado->distProxEstacao!=-2) r->distProxEstacao=atualizado->distProxEstacao;
             if(atualizado->codLinhaIntegra!=-2) r->codLinhaIntegra= atualizado->codLinhaIntegra;
@@ -212,13 +172,12 @@ void aplicacao6(FILE* file){
             if(strcmp(atualizado->nomeEstacao,"Sem restrição")!=0) strcpy(r->nomeEstacao,atualizado->nomeEstacao);
             if(strcmp(atualizado->nomeLinha,"Sem restrição")!=0) strcpy(r->nomeLinha,atualizado->nomeLinha);
             int tamanho_atualizado= 34+ strlen(r->nomeEstacao)+ strlen(r->nomeLinha);
-            //imprimeRegistro(r);
-            if(tamanho_atualizado<=r->tamanhoRegistro){
-                fseek(file,-r->tamanhoRegistro-5,SEEK_CUR);
+            if(tamanho_atualizado<=r->tamanhoRegistro){//cabe nesse registro a atualizacao
+                fseek(file,-r->tamanhoRegistro-5,SEEK_CUR);//volta ao inicio do registro
                 insereg(file,r,0);}             
-            else{
-                r->removido='1';
-                r->proxLista=ca.topoLista;
+            else{//nao cabe nesse
+                r->removido='1';//marca como removido
+                r->proxLista=ca.topoLista;//poe no topo da lista
                 long int fim_regis=ftell(file);
                 ca.topoLista = fim_regis-r->tamanhoRegistro -5;
                 fseek(file,ca.topoLista,SEEK_SET);
